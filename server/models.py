a8940ccc1161f31564004e0af389e477415b1a99
Sofia Katzman <katzmansof@gmail.com>
2023-06-27 21:54:24 -0400

    add user table to initialize db

--- server/models.py
+++ server/models.py
@@ -1,5 +1,11 @@
 from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy 
 
 from config import db
 
-# Models go here!
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key = True)
+
+
