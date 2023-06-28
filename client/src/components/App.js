3d4eb7362ea89b9a416d4bc336385f5851579f3a
Sofia Katzman <katzmansof@gmail.com>
2023-06-28 01:40:48 -0400

    add css sheet

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,11 +5,12 @@ import Home from "./Home";
 import Friends from "./Friends";
 import Create from "./Create";
 import Settings from "./Settings";
+import './index.css'
 
 function App() {
   return (
     <>
-      <h1>milestones</h1>
+      <h1 className="title">milestones</h1>
       <div>
         <Router>
           <Navigation />
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,11 +6,9 @@ function Home(){
     return(
         <>
         <h1>Welcome to milestones!</h1>
-        <button>LOGIN</button>
-        <button>NEW USER</button>
-
-        <h1><u>If logged in: </u></h1>
-        <Milestones/>
+        <button>LOGIN</button> &nbsp; <button>NEW USER</button>
+        {/* <h1><u>If logged in: </u></h1>
+        <Milestones/> */}
         </>
     )
 }
