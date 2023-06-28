4cf3bec22d11ac74febf8473cc9a18fcfed746a8
Sofia Katzman <katzmansof@gmail.com>
2023-06-28 01:41:06 -0400

    create basic navigation routes

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,20 +1,19 @@
-import React from "react"
-import { Link } from 'react-router-dom'
+import React from "react" 
+import { Link } from 'react-router-dom' 
 
-function Navigation(){
-    return(
-        <nav className="links">
-            <Link to="/">home</Link><br/>
-            <Link to="/friends">friends</Link><br/>
-            <Link to="/create">new milestone</Link><br/>
-            <Link to="/settings">settings</Link><br/>
-        </nav>
-    )
-
-
+function Navigation() {
+  return (
+    <>
+    <nav className="navbar">
+      <h1 className="icon">☰</h1>
+      <div className="links">
+        <Link to="/">home</Link><br />
+        <Link to="/friends">friends</Link><br />
+        <Link to="/create">new</Link><br />
+        <Link to="/settings">settings</Link><br />
+      </div>
+    </nav></>
+  ) 
 }
-
-
-
 
 export default Navigation 
