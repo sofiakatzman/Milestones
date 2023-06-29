e2fc32ca39c4c0a02392553ca600436f70f6fabe
Sofia Katzman <katzmansof@gmail.com>
2023-06-29 01:20:05 -0400

    removed milestones from navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -5,10 +5,9 @@ function Navigation() {
   return (
     <>
     <nav className="navbar">
-      <h1 className="icon">☰</h1>
+      <h1 className="icon">☲</h1>
       <div className="links">
         <Link to="/">home</Link><br />
-        <Link to="/timeline">milestones</Link><br />
         <Link to="/friends">friends</Link><br />
         <Link to="/create">new</Link><br />
         <Link to="/settings">settings</Link><br />
