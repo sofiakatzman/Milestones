d0e50d874e625c7cb53ccc8b0844d0cfae196f88
Sofia <katzmansof@gmail.com>
2023-06-30 17:12:56 -0400

    update dependency and replace router switch

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,32 +1,31 @@
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./Navigation";
 import Home from "./Home";
 import Friends from "./Friends";
-import Create from "./Create"
-import Settings from "./Settings"
-import Milestones from "./Milestones"
-import Log from './Log'
-import './index.css'
+import Create from "./Create";
+import Settings from "./Settings";
+import Milestones from "./Milestones";
+import Log from './Log';
+import './index.css';
+
 
 function App() {
 
+
   return (
-    <>
-      <div>
-        <Log/>
-        <Router>
-          <Navigation />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/friends" component={Friends} />
-            <Route exact path="/create" component={Create} />
-            <Route exact path="/settings" component={Settings} />
-            <Route exact path="/timeline" component={Milestones} />
-          </Switch>
-        </Router>
-      </div>
-    </>
+    <div>
+      <Log />
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/friends" element={<Friends />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/timeline" element={<Milestones />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
