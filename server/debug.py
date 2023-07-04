e212734121a7ba9b420415b256ff4711d5cd9ac3
Sofia Katzman <katzmansof@gmail.com>
2023-07-04 01:35:03 -0400

    add debugger script

---
+++ server/debug.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from models import (Milestone, User, Aspect)
+
+if __name__ == '__main__':
+    engine = create_engine("sqlite:///event_manager.db")
+    session = Session(engine, future=True)
+
+    import ipdb; ipdb.set_trace()
