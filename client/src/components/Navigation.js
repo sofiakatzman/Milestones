ff45eed31bf1cc647962c6db257727d0dcd1ae00
Sofia Katzman <katzmansof@gmail.com>
2023-07-07 12:47:01 -0400

    add state to navigation for login component

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,16 +1,17 @@
 import { Link } from 'react-router-dom' 
+import Log from './Log'
 
-function Navigation() {
+function Navigation({updateUser, user}) {
   return (
     <>
     <nav className="navbar">
+    <Log updateUser={updateUser} user={user}/>
       <h1 className="icon">☲</h1>
       <div className="links">
         <Link to="/">home</Link><br />
         <Link to="/friends">friends</Link><br />
         <Link to="/create">new</Link><br />
         <Link to="/settings">settings</Link><br />
-
       </div>
     </nav></>
   ) 
