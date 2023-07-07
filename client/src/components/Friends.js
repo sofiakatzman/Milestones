c51c512e7b5dbff2d5bc8f4d55e5e767b954ffb9
Sofia Katzman <katzmansof@gmail.com>
2023-07-07 19:14:19 -0400

    add friends fetch logic and map display

--- client/src/components/Friends.js
+++ client/src/components/Friends.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react"
+
 function Friends(){
+
+    const [friends, setFriends] = useState(null)
+
+    useEffect(()=> {
+        fetch("http://127.0.0.1:5000/friends")
+        .then(r => r.json()).then(data => setFriends(data))
+    })
+
+
+
+
     return(
         <>
         <h1>These are all of your friends!</h1>
+
+        {friends && friends.map(friend => {
+            return(
+                <div>
+                Friend Username: {friend.friend.username} <br/>
+                Friend Birthday: {friend.friend.birthday} <br/> <br/>
+                </div>
+            )
+        })}
         </>
     )
 }
