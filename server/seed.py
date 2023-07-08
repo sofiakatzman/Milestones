04174e7bcb77e0be26aa9ba82618a9f262b48e62
Sofia Katzman <katzmansof@gmail.com>
2023-07-07 22:43:57 -0400

    add back join table

--- server/seed.py
+++ server/seed.py
@@ -1,47 +1,44 @@
 from random import randint
 from faker import Faker
-import random
 
 from app import app
-from models import db, User, Milestone, Aspect, Friend
+from models import db, User, Milestone, Aspect, user_aspects, Friend
 
 def seed():
-    # Delete current data from tables
+    fake = Faker()
 
-    User.query.delete()
+    # Delete current data from tables
+    db.session.query(user_aspects).delete()
+    Friend.query.delete()
     Milestone.query.delete()
     Aspect.query.delete()
-
-
-    fake = Faker()
+    User.query.delete()
 
     # Create mock user data
     for i in range(10):
         new_user = User(
-            id=i + 1,
-            birthday=fake.passport_dob(),
+            birthday=fake.date_of_birth(minimum_age=18, maximum_age=90),
             username=(fake.first_name() + fake.last_name()).lower(),
             _password_hash="temporary"
         )
         db.session.add(new_user)
+        db.session.flush()  # Flush the session to obtain the generated user ID
 
         new_milestone = Milestone(
-            id=i + 1,
             date=fake.date_this_century(),
             header=fake.text(max_nb_chars=20),
             subheader=fake.text(max_nb_chars=30),
             description=fake.text(max_nb_chars=70),
             is_private=fake.pybool(),
-            user_id=i + 1,
-            aspect_id=random.randint(1, 4)
+            user_id=new_user.id,
+            aspect_id=randint(1, 4)
         )
         db.session.add(new_milestone)
 
-    # Add friends to users 
-    # Retrieve all users from the database
-    users = User.query.all() 
+    # Add friends to users
+    users = User.query.all()
     for user in users:
-        num_friends = randint(1, 10)  # Randomly determine the number of friends for each user
+        num_friends = randint(1, 5)  # Randomly determine the number of friends for each user
         user_friends = []
 
         # Randomly select friends from all users
@@ -52,21 +49,19 @@ def seed():
 
         # Create friendship objects and add them to the session
         for friend in user_friends:
-            friendship = Friend(user=user, friend=friend)
+            friendship = Friend(user_id=user.id, friend_id=friend.id)
             db.session.add(friendship)
-
 
     # Add more milestones to users
     for i in range(300):
         new_milestone = Milestone(
-            id=i + 11,
             date=fake.date_this_century(),
             header=fake.text(max_nb_chars=20),
             subheader=fake.text(max_nb_chars=30),
             description=fake.text(max_nb_chars=70),
             is_private=fake.pybool(),
-            user_id=random.randint(1, 10),
-            aspect_id=random.randint(1, 5)
+            user_id=randint(1, 10),
+            aspect_id=randint(1, 4)
         )
         db.session.add(new_milestone)
 
@@ -88,6 +83,10 @@ def seed():
         )
         db.session.add(new_aspect)
 
+    # Create instances of UserAspect
+    for i in range(1, 50):
+        new_instance = user_aspects.insert().values(user_id=i, aspect_id=2)
+        db.session.execute(new_instance)
 
     # Commit changes to the session
     db.session.commit()
