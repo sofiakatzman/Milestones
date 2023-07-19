d39067b4cb17772db9580bc608c25a6ba7b311e7
Sofia Katzman <katzmansof@gmail.com>
2023-07-19 01:38:22 -0400

    finalize logic for delete user and patch username settings

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -2,40 +2,43 @@ import React from 'react'
 import { useFormik } from 'formik'
 import * as yup from 'yup'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-function Settings() {
-  
+function Settings({user, updatedUser}) {
+  const navigate = useNavigate()
   const [editable, setEditable] = useState({
     username: false,
     birthday: false,
     // password: false,
   })
 
-  const formSchema = yup.object().shape({
-    username: yup.string().required('Please enter a username'),
-    // password: yup.string().required('Please enter a password'),
-    birthday: yup.date().required('Please enter your birthdate.'),
-  })
+  // const formSchema = yup.object().shape({
+  //   username: yup.string().required('Please enter a username'),
+  // })
 
   const formik = useFormik({
     initialValues: {
-      username: '',
-      birthday: '',
-      // password: '',
+      username: user.username, // Initialize with the current username
     },
-    validationSchema: formSchema,
+    // ... your other formik configurations ...
     onSubmit: (values) => {
-      console.log(values)
-      // Make separate fetch requests to patch individual fields based on logged in user's id
-      if (editable.username) {
-        // fetch request to update username
-      }
-      // if (editable.password) {
-      //   // fetch request to update password
-      // }
-      if (editable.birthday) {
-        // fetch request to update birthday
-      }
+      fetch(`http://localhost:5000/users/${user.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username: values.username }), // Use formik.values.username
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log(`User ${user.id} has been updated.`)
+          } else {
+            console.error("Failed to update user.")
+          }
+        })
+        .catch((error) => {
+          console.error("Error updating user:", error)
+        })
     },
   })
 
@@ -46,17 +49,23 @@ function Settings() {
     }))
   }
 
-  const deleteAccount = () => {
+  const deleteAccount = (userID) => {
     //add css to this button so its red and an erro
-    //function will delete user account -> send delete request to user 
     
   }
+
+  const editUsername = (userID, updatedUser) => {
+    fetch(`http://localhost:5000/users/${userID}`, { 
+
+    }).then(r=> r.json()).then(res=> console.log(res)) 
+  }
+
   return (
     <>
-      <h1>User settings...</h1>
-      {/* <h2>Hello, {user.username}</h2> */}
+      <h1>edit user settings</h1>
+      {/* <h3>Hello, {user.username}</h3> */}
       <form onSubmit={formik.handleSubmit}>
-        <label>Username </label>
+        <label>username : {user.username} </label> 
         {editable.username ? (
           <>
             <input
@@ -79,56 +88,10 @@ function Settings() {
         )}
         <br />
         <br />
-        {/* <label>Password </label>
-        {editable.password ? (
-          <>
-            <input
-              type="password"
-              name="password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-            />
-            <button type="button" onClick={() => toggleEditable('password')}>
-              Cancel
-            </button>
-          </>
-        ) : (
-          <>
-            <span>{formik.values.password}</span>
-            <button type="button" onClick={() => toggleEditable('password')}>
-              Edit
-            </button>
-          </>
-        )}
-        <br />
-        <br /> */}
-        <label>Birthdate </label>
-        {editable.birthday ? (
-          <>
-            <input
-              type="date"
-              name="birthday"
-              value={formik.values.birthday}
-              onChange={formik.handleChange}
-            />
-            <button type="button" onClick={() => toggleEditable('birthday')}>
-              Cancel
-            </button>
-          </>
-        ) : (
-          <>
-            <span>{formik.values.birthday}</span>
-            <button type="button" onClick={() => toggleEditable('birthday')}>
-              Edit
-            </button>
-          </>
-        )}
-        <br />
-        <br />
-        {/* {editable.username || editable.password || editable.birthday ? (
-          <button type="submit">Save Changes</button>
-        ) : null} */}
-        <button className="delete" onClick={deleteAccount}>DELETE ACCOUNT</button>
+        {editable.username ? (
+          <button type="submit">Save Changes</button> 
+        ) : null}<br/>
+        <button className="delete" onClick={()=>deleteAccount(user.id)}>DELETE ACCOUNT</button>
       </form>
     </>
   )
