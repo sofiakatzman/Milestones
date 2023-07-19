792d54cba315c9e18125c952492267a2d397a9f8
Sofia Katzman <katzmansof@gmail.com>
2023-07-18 22:34:08 -0400

    remove logout from navbar and add create new aspect link

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -4,16 +4,16 @@ import ReactDOM from 'react-dom/client'
 import NavBar from './NavBar'
 import React from 'react';
 
-function Navigation({updateUser, user}) {
+function Navigation() {
   return (
     <>
      <React.StrictMode>
     <NavBar side='right' bgColor='black' burgerSize={30} hamColorClosed='black' hamColorOpen='white'> 
-    <Log updateUser={updateUser} user={user}/>
       <div className="links"> 
         <Link to="/">home</Link><br />
         <Link to="/friends">friends</Link><br />
         <Link to="/create">new</Link><br />
+        <Link to="/aspects">aspects</Link> <br/>
         <Link to="/settings">settings</Link><br />
         </div>
     </NavBar></React.StrictMode> </>
