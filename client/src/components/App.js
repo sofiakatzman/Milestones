84f354cc67ea49d5b967a6343dc0b34aa8b72cad
Sofia Katzman <katzmansof@gmail.com>
2023-07-18 22:32:23 -0400

    add aspect path & add user_id state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,46 +9,55 @@ import Log from './Log'
 import './index.css'
 import { useEffect, useState } from "react"
 import FriendMilestones from "./FriendMilestones"
+import Authentication from "./Authentication"
+import Aspects from "./Aspects"
 
 function App() {
   const [user, setUser] = useState(null)
-  console.log(user)
+  const [user_id, setUserID] = useState(0)
 
   useEffect(()=>{
-    fetchUser()
-  },[])
-  
-  const updateUser = (update) => {
-    setUser(update)
-  }
-
-  const fetchUser = () => {
     fetch('http://localhost:5000/authorized')
     .then(res => {
       if(res.ok){
-        res.json().then(user => setUser(user))
+        res.json().then(user => {
+          setUser(user)
+          setUserID(user.id)
+        })
       }else {
         setUser(null)
       }
     })
-}
+  },[])
+  
+  const updateUser = (update) => {
+    setUser(update)
+    if (update!= null){
+      setUserID(update.id)
+    }
+  }
 
-
-
-
+  if(!user)
+    return(
+      <>
+      <Router>
+        <Authentication updateUser={updateUser} />
+      </Router>
+      </>
+  )
   return (
     <div>
       <Router>
       <Log updateUser={updateUser} /> 
-      
         <Navigation />
         <Routes>
-          <Route path="/" element={<Home updateUser={updateUser}/>} />
-          <Route path="/friends" element={<Friends user={user} />} />
-          <Route path="/create" element={<Create />} />
+          <Route path="/" element={<Home updateUser={updateUser} user_id={user_id}/>} />
+          <Route path="/friends" element={<Friends user_id={user_id}/>} />
+          <Route path="/create" element={<Create user_id={user_id}/>} />
+          <Route path="/aspects" element={<Aspects />} />
           <Route path="/settings" element={<Settings user={user}/>} />
-          <Route path="/timelines" element={<Milestones />} />
-          <Route path="/timelines/:user_id" element={<FriendMilestones />} />
+          <Route path="/timelines" element={<Milestones user={user} />} />
+          <Route path="/timelines/:user_id" element={<FriendMilestones user={user}/>} />
         </Routes>
       </Router>
     </div>
